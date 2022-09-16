a18fdb16448979961831779c55ee85ac52a34320
pmk7 <72909830+pmk7@users.noreply.github.com>
2022-09-16 12:54:25 +0200

    setup routes to dashboard, register, landing, error pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import Landing from './pages/Landing';
+import { Landing, Dashboard, Register, Error } from './pages';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to='/'>Dashboard</Link>
-        <Link to='/register'>Register</Link>
-        <Link to='/landing'>Landing</Link>
-      </nav>
       <Routes>
-        <Route path='/' element={<Landing />} />
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
