405d913dd010dc5e7ae46a75fb6c62a443823827
pmk7 <72909830+pmk7@users.noreply.github.com>
2022-09-23 14:01:31 +0200

    added error page component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
-  return <h1>Error</h1>;
+  return (
+    <Wrapper classname='full-page'>
+      <div>
+        <img src={img} alt='not found' />
+        <h3>404</h3>
+        <p>oops page not found</p>
+        <Link to='/'>back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
