b377493b517e64c4a1308cbc0cbb419fbfe460ae
pmk7 <72909830+pmk7@users.noreply.github.com>
2022-09-28 16:46:29 +0200

    added error, dashboard, register pages

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
-  return <h1>Error</h1>;
+  return (
+    <Wrapper classname='full-page'>
+      <div>
+        <img src={img} alt='not found' />
+        <h1>oops</h1>
+        <p>page not found</p>
+        <Link to='/'>back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
