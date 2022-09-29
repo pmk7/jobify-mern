bd2cd6c44f15b964c59bc31b8d78edb8efa38e86
pmk7 <72909830+pmk7@users.noreply.github.com>
2022-09-29 20:02:03 +0200

    register component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initalState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
 
 const Register = () => {
-  return <h1>Register</h1>;
+  const [values, setValues] = useState(initalState);
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={handleSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* name field */}
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            {' '}
+            name
+          </label>
+          <input
+            type='text'
+            value={values.name}
+            name='name'
+            onChange={handleChange}
+            className='form-input'
+          />
+        </div>
+        <button className='btn btn-block'></button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
