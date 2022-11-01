565c2b93c18e7e0fd4d3f29e925e46e9333541e3
pmk7 <72909830+pmk7@users.noreply.github.com>
2022-11-01 15:37:25 +0100

    set up user schema using mongoose

---
+++ models/User.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minLength: 3,
+    maxLength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minLength: 6,
+    unique: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxLength: 20,
+    default: 'lastName',
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxLength: 20,
+    default: 'my city',
+  },
+});
+
+export default mongoose.model('User', UserSchema);
