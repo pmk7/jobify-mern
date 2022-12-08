e09c1bf1295e69c5e7c8606f7272d061ac7dc586
pmk7 <72909830+pmk7@users.noreply.github.com>
2022-12-08 15:47:55 +0100

    checking for email when new user registers, handling error if email isn't unique

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,31 +2,17 @@ import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError } from '../errors/index.js';
 
-// class CustomAPIError extends Error {
-//   constructor(message) {
-//     super(message);
-//   }
-// }
-
-// class BadRequestError extends CustomAPIError {
-//   constructor(message) {
-//     super(message);
-//     this.statusCode = StatusCodes.BAD_REQUEST;
-//   }
-// }
-// class NotFoundError extends CustomAPIError {
-//   constructor(message) {
-//     super(message);
-//     this.statusCode = StatusCodes.NOT_FOUND;
-//   }
-// }
-
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     throw new BadRequestError('please include all required fields');
   }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email already in use');
+  }
+
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
