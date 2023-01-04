bfbda30ecfa5fb42773afc55c439e885122a8263
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-01-04 11:22:13 +0100

    refactored register user. Created single setup user function

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,12 +1,9 @@
 import {
   DISPLAY_ALERT,
   CLEAR_ALERT,
-  REGISTER_USER_BEGIN,
-  REGISTER_USER_SUCCESS,
-  REGISTER_USER_ERROR,
-  LOGIN_USER_BEGIN,
-  LOGIN_USER_SUCCESS,
-  LOGIN_USER_ERROR,
+  SETUP_USER_BEGIN,
+  SETUP_USER_SUCCESS,
+  SETUP_USER_ERROR,
 } from '../actions.js';
 
 const reducer = (state, action) => {
@@ -27,10 +24,13 @@ const reducer = (state, action) => {
       alertType: '',
     };
   }
-  if (action.type === REGISTER_USER_BEGIN) {
+
+  if (action.type === SETUP_USER_BEGIN) {
     return { ...state, isLoading: true };
   }
-  if (action.type === REGISTER_USER_SUCCESS) {
+
+  if (action.type === SETUP_USER_SUCCESS) {
+    console.log(SETUP_USER_BEGIN);
     return {
       ...state,
       isLoading: false,
@@ -40,38 +40,11 @@ const reducer = (state, action) => {
       jobLocation: action.payload.location,
       showAlert: true,
       alertType: 'success',
-      alertText: 'User Created! Redirecting...',
-    };
-  }
-  if (action.type === REGISTER_USER_ERROR) {
-    return {
-      ...state,
-      isLoading: false,
-      showAlert: true,
-      alertType: 'danger',
-      alertText: action.payload.msg,
+      alertText: action.payload.alertText,
     };
   }
 
-  if (action.type === LOGIN_USER_BEGIN) {
-    return { ...state, isLoading: true };
-  }
-
-  if (action.type === LOGIN_USER_SUCCESS) {
-    return {
-      ...state,
-      isLoading: false,
-      token: action.payload.token,
-      user: action.payload.user,
-      userLocation: action.payload.location,
-      jobLocation: action.payload.location,
-      showAlert: true,
-      alertType: 'success',
-      alertText: 'Login successful! Redirecting...',
-    };
-  }
-
-  if (action.type === LOGIN_USER_ERROR) {
+  if (action.type === SETUP_USER_ERROR) {
     return {
       ...state,
       isLoading: false,
