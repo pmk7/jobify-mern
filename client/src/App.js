4cc78da654e42dabadb7148b66db9ad86232562e
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-01-05 12:29:36 +0100

    created nested homepage layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import { Landing, Dashboard, Register, Error } from './pages';
+import { Landing, Register, Error } from './pages';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {
+  Stats,
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+} from './pages/dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
