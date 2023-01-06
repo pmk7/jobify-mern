ab51c579e580ede74aa6a914b52583ee0ab5ce48
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-01-06 17:50:29 +0100

    setting up shared layout dashboard with nested components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,8 @@
 import Alert from './Alert';
 import FormRow from './FormRow';
 import Logo from './Logo';
+import Navbar from '../components/Navbar';
+import BigSidebar from '../components/BigSidebar';
+import SmallSidebar from '../components/SmallSidebar';
 
-export { Logo, FormRow, Alert };
+export { Logo, FormRow, Alert, Navbar, BigSidebar, SmallSidebar };
