cee18ad0ea97914a10974b024761ffd8dfa14fd1
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-01-08 21:03:00 +0100

    set up navbar  styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Wrapper from '../assets/wrappers/Navbar';
-import { FaHome } from 'react-icons/fa';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import Logo from './Logo';
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
-      <FaHome />
+      <div className='nav-center'>
+        <button
+          className='toggle-btn'
+          onClick={() => console.log('toggle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button className='btn' onClick={() => console.log('show logout')}>
+            <FaUserCircle />
+            phil
+            <FaCaretDown />
+          </button>
+          <div className='dropdown show-dropdown'>
+            <button
+              className='dropdown-btn'
+              onClick={() => console.log('logout user')}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
