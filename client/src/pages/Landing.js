e7626b786a443c8399a02d600c41ee16644c2b96
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-01-10 19:04:34 +0100

    set up links on small sidebar to stats, all jobs, add jobs and profile pages

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -10,24 +10,24 @@ const Landing = () => {
     <Wrapper>
       <nav>
         <Logo />
-        <div className='container page'>
-          <div className='info'>
-            <h1>
-              job <span>tracking</span>app
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-              amet incidunt earum totam neque alias dolore, debitis est quas
-              ipsum placeat consequatur nesciunt deleniti quaerat repellat illum
-              nam, aliquid ducimus!
-            </p>
-            <Link to='/register' className='btn btn-hero'>
-              Login/Register
-            </Link>
-          </div>
-          <img src={main} alt='job hunt' className='img main-img' />
-        </div>
       </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span>app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
+            amet incidunt earum totam neque alias dolore, debitis est quas ipsum
+            placeat consequatur nesciunt deleniti quaerat repellat illum nam,
+            aliquid ducimus!
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
     </Wrapper>
   );
 };
