4d174d08c5417c1044ff9c90c03ebc0da14b3b00
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-01-11 15:14:39 +0100

    set up nav links as seperate component, rendered in sidebar components

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/BigSidebar';
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
+import { useAppContext } from '../context/appContext';
 
 const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>Big Sidebar</h4>
+      <div
+        className={
+          showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -2,9 +2,8 @@ import React from 'react';
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import { FaTimes } from 'react-icons/fa';
 import { useAppContext } from '../context/appContext';
-import links from '../utils/links';
-import { NavLink } from 'react-router-dom';
 import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const SmallSidebar = () => {
   const { toggleSidebar, showSidebar } = useAppContext();
@@ -23,22 +22,7 @@ const SmallSidebar = () => {
             <Logo />
           </header>
           <div className='nav-links'>
-            {links.map((link) => {
-              const { text, path, id, icon } = link;
-              return (
-                <NavLink
-                  to={path}
-                  className={({ isActive }) =>
-                    isActive ? 'nav-link active' : 'nav-link'
-                  }
-                  key={id}
-                  onClick={toggleSidebar}
-                >
-                  <span className='icon'>{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
+            <NavLinks toggleSidebar={toggleSidebar} />
           </div>
         </div>
       </div>
