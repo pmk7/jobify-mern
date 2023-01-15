68f33f704347f7cbab60dd6436221b2c2db2d42e
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-01-15 16:08:27 +0100

    set up auth function middleware to verify user has jwt token in order to access data

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,19 @@
+import jwt from 'jsonwebtoken';
+import { UnauthenticatedError } from '../errors/index.js';
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log(authHeader);
-  next();
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: payload.userId };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
 };
 
 export default auth;
