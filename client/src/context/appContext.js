79fcd8e67693c1e5068420ef9bb48eaa8e49a141
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-01-22 15:20:46 +0000

    set up axios interceptor to ensure user has bearer token and is authorizsed to access data

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -32,6 +32,36 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // axios custom instance
+  const authFetch = axios.create({
+    baseURL: '/api/v1',
+  });
+
+  // request
+  authFetch.interceptors.request.use(
+    (config) => {
+      config.headers['Authorization'] = `Bearer ${state.token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  // response
+  authFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      console.log(error.response);
+      if (error.response.status === 401) {
+        console.log('AUTH ERROR');
+      }
+      return Promise.reject(error);
+    }
+  );
+
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
     clearAlert();
@@ -87,21 +117,14 @@ const AppProvider = ({ children }) => {
   };
 
   const logoutUser = () => {
+    console.log(initialState);
     dispatch({ LOGOUT_USER });
     removeUserFromLocalStorage();
   };
 
   const updateUser = async (currentUser) => {
     try {
-      const { data } = await axios.patch(
-        '/api/v1/auth/updateUser',
-        currentUser,
-        {
-          headers: {
-            Authorization: `Bearer ${state.token}`,
-          },
-        }
-      );
+      const { data } = await authFetch.patch('/auth/updateUser', currentUser);
       console.log(state.token);
       console.log(data);
     } catch (error) {
